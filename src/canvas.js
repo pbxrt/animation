72e616c75ef7c33ba6ff20d394434f2afcf78400
plearner <953413537@qq.com>
2017-06-06 19:41:14 +0800

    a moving ball

--- src/canvas.js
+++ src/canvas.js
@@ -3,9 +3,11 @@ canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 let ctx = canvas.getContext('2d')
 
-function Ball(x,y,radius,color){
+function Ball(x, dx, y, dy, radius,color){
   this.x = x
+  this.dx = dx
   this.y = y
+  this.dy = dy
   this.radius = radius
   this.color = color
   ctx.beginPath()
@@ -13,5 +15,21 @@ function Ball(x,y,radius,color){
   ctx.fillStyle = this.color
   ctx.fill()
 }
+Ball.prototype.update = function(){
+  this.x += this.dx
+  this.y += this.dy
+  ctx.beginPath()
+  ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2, true)
+  ctx.fillStyle = this.color
+  ctx.fill()
+}
 
-var ball = new Ball(200, 200, 100, '#ccc');
+var ball = new Ball(200, 1, 200, 1, 100, '#ccc');
+
+function animate(){
+  requestAnimationFrame(animate)
+  ctx.clearRect(0, 0, innerWidth, innerHeight)
+  ball.update()
+}
+
+animate()
