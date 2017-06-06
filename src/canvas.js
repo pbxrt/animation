5fc8e59d1a1d043668406fc25f877578f2e5d44f
plearner <953413537@qq.com>
2017-06-06 20:06:24 +0800

    改变ball的半径

--- src/canvas.js
+++ src/canvas.js
@@ -31,12 +31,18 @@ Ball.prototype = {
   //每次更新ball的坐标
   update: function(){
     this.x += this.dx
+    //如果ball的x坐标超出屏幕，让他反向运动
     if(this.x + this.radius >= innerWidth || this.x - this.radius <= 0){
       this.dx = -this.dx
     }
     this.y += this.dy
+    //如果ball的y坐标超出屏幕，让他反向运动
     if(this.y + this.radius >= innerHeight || this.y - this.radius <= 0){
       this.dy = -this.dy
+    }
+    //如果ball的x坐标距离鼠标的x坐标不超过50的话，让它的半径增1
+    if(mouse.x - this.x < 50 && mouse.x - this.x > -50){
+      this.radius += 1
     }
     this.draw()
   }
