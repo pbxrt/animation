0a13bd00372183c44afc08910e3d686814d6ff89
plearner <953413537@qq.com>
2017-06-06 20:02:14 +0800

    记录鼠标的位置

--- src/canvas.js
+++ src/canvas.js
@@ -3,6 +3,15 @@ canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 let ctx = canvas.getContext('2d')
 
+let mouse = {
+  x: undefined,
+  y: undefined
+}
+window.addEventListener('mousemove', function(e){
+  mouse.x = e.x
+  mouse.y = e.y
+})
+
 function Ball(x, dx, y, dy, radius,color){
   this.x = x
   this.dx = dx
@@ -12,12 +21,14 @@ function Ball(x, dx, y, dy, radius,color){
   this.color = color
 }
 Ball.prototype = {
+  //每次重新绘制的时候都要调用的方法
   draw: function(){
     ctx.beginPath()
     ctx.arc(this.x, this.y ,this.radius, 0, Math.PI*2, true)
     ctx.fillStyle = this.color
     ctx.fill()
   },
+  //每次更新ball的坐标
   update: function(){
     this.x += this.dx
     if(this.x + this.radius >= innerWidth || this.x - this.radius <= 0){
@@ -34,12 +45,13 @@ Ball.prototype = {
 
 var ballArr = []
 for(var i=0; i<10; i++){
-  var radius = 10 + Math.random()*40
+  var radius = 3 + Math.random()*10
   var dx = Math.random()*10 - 5
   var dy = Math.random()*10 - 5
   var ball = new Ball(200, dx, 200, dy, radius, '#ccc');
   ballArr.push(ball)
 }
+
 function animate(){
   requestAnimationFrame(animate)
   ctx.clearRect(0, 0, innerWidth, innerHeight)
