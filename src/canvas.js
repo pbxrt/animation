819a88a35d2f4d2b66ff62ec612d038571f6447d
plearner <953413537@qq.com>
2017-06-06 19:33:23 +0800

    create a ball

--- src/canvas.js
+++ src/canvas.js
@@ -2,5 +2,16 @@ let canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 let ctx = canvas.getContext('2d')
-ctx.font = '48px serif'
-ctx.fillText('Hello world!', 100, 100)
+
+function Ball(x,y,radius,color){
+  this.x = x
+  this.y = y
+  this.radius = radius
+  this.color = color
+  ctx.beginPath()
+  ctx.arc(this.x, this.y ,this.radius, 0, Math.PI*2, true)
+  ctx.fillStyle = this.color
+  ctx.fill()
+}
+
+var ball = new Ball(200, 200, 100, '#ccc');
